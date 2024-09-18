03b35bd29f5a35adeef1729d1933b5c475b52ed0
JoshuaPruitt <JoshuaPruitt6484@gmail.com>
2024-09-17 21:35:47 -0600

    added mouse hold functionality

--- client/src/pages/canvasPage.tsx
+++ client/src/pages/canvasPage.tsx
@@ -1,4 +1,4 @@
-import { useRef, useEffect} from "react"
+import { useRef, useState, useEffect} from "react"
 
 const CanvasComponent: React.FC = () => {
     const canvasRef = useRef<HTMLCanvasElement | null>(null);
@@ -6,6 +6,9 @@ const CanvasComponent: React.FC = () => {
     const canvasHeight = 500;
     const pixelSize = 10;
 
+    const [isPainting, setPainting] = useState(false);
+
+    //draws an initial grid onto the canvas.
     const drawGrid = (ctx: CanvasRenderingContext2D) => {
         for (let x= 0; x< canvasWidth; x += pixelSize){
             for(let y=0; y < canvasHeight; y += pixelSize){
@@ -18,7 +21,38 @@ const CanvasComponent: React.FC = () => {
         }
     };
 
-    const handleCanvasClick = (event: React.MouseEvent<HTMLCanvasElement>) => {
+    const drawPixel = (ctx: CanvasRenderingContext2D, x: number, y: number) => {
+        //grabs the mouse click coordinates
+        const pixelX = Math.floor(x /pixelSize) * pixelSize;
+        const pixelY = Math.floor(y /pixelSize) * pixelSize;
+
+        const currentColor = ctx.getImageData(pixelX, pixelY, 1, 1).data;
+        const isRed = currentColor[0] === 255 && currentColor[1] === 0 && currentColor[2] === 0;
+
+        //between red and black currently
+        ctx.fillStyle = isRed ? '#000000' : '#ff0000';
+        ctx.fillRect(pixelX, pixelY, pixelSize, pixelSize);
+    }
+
+    const handleMouseDown = (event: React.MouseEvent<HTMLCanvasElement>) => {
+        const canvas = canvasRef.current;
+        if(!canvas) return;
+
+        const ctx = canvas.getContext('2d');
+        if(!ctx) return;
+
+        // Grabs the canvas's bounding retangle and the mouse click Coordinates
+        const rect = canvas.getBoundingClientRect();
+        const x = event.clientX - rect.left;
+        const y = event.clientY - rect.top;
+
+        drawPixel(ctx, x, y);
+        setPainting(true);
+    }
+
+    const handleMouseMove = (event: React.MouseEvent<HTMLCanvasElement>) => {
+        if(!isPainting) return;
+
         const canvas = canvasRef.current;
         if(!canvas) return;
 
@@ -29,15 +63,12 @@ const CanvasComponent: React.FC = () => {
         const x = event.clientX - rect.left;
         const y = event.clientY - rect.top;
 
-        const pixelX = Math.floor(x / pixelSize) * pixelSize;
-        const pixelY = Math.floor(y / pixelSize) * pixelSize;
-
-        const currentColor = ctx.getImageData(pixelX, pixelY, 1, 1).data;
-        const isRed = currentColor[0] === 255 && currentColor[1] === 0 && currentColor[2] === 0;
-
-        ctx.fillStyle = isRed ? '#000000' : '#ff0000';
-        ctx.fillRect(pixelX, pixelY, pixelSize, pixelSize);
+        drawPixel(ctx, x, y)
     }
+
+    const handleMouseUpLeave = () => {
+        setPainting(false);
+    };
 
     useEffect(() => {
         const canvas = canvasRef.current;
@@ -54,7 +85,7 @@ const CanvasComponent: React.FC = () => {
             <p>Canvas Page</p>
 
             <div className="canvasHolder">
-                <canvas id='paintMain' ref={canvasRef} width={canvasWidth} height={canvasHeight} onClick={handleCanvasClick} style={{border: '1px solid red'}}></canvas>
+                <canvas id='paintMain' ref={canvasRef} width={canvasWidth} height={canvasHeight} onMouseDown={handleMouseDown} onMouseMove={handleMouseMove} onMouseUp={handleMouseUpLeave} onMouseLeave={handleMouseUpLeave}style={{border: '1px solid red'}}></canvas>
             </div>
         </div>
     )
