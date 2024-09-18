a338be9a3c392a41767b1bf2740f8999b173c978
Papito <170674313+PeruvPowWow@users.noreply.github.com>
2024-09-18 16:02:20 -0600

    added the const to the homepage for logs, projects, loading and errors

--- client/src/pages/homePage.tsx
+++ client/src/pages/homePage.tsx
@@ -2,3 +2,10 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 import Navbar from '../components/Navbar';
+
+const Home: React.FC = () => {
+    const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
+    const [projecsts, setProjects] = useState<any[]>([]);
+    const [loading, setLoading] = useState<boolean>(true);
+    const [error, setError] = useState<string | null>(null);
+    
