1baa2ad7a42aeef50cac787ffe8ef122335a205d
Papito <170674313+PeruvPowWow@users.noreply.github.com>
2024-09-20 00:20:35 -0400

    adding the imports to the page and then putting the elements of the signup page

--- client/src/pages/signupPage.tsx
+++ client/src/pages/signupPage.tsx
@@ -1,3 +1,10 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
+
+const SignUpPage = () => {
+    const [email, setEmail] = useState('');
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [errorMessage, setErrorMessage] = useState('');
+    const navigate = useNavigate();
