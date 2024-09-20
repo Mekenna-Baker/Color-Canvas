c6a8e3e5244ff5389f7cfa7defef7a33077d2db6
Papito <170674313+PeruvPowWow@users.noreply.github.com>
2024-09-20 00:25:19 -0400

    added the function to validate the email data for the sign up page

--- client/src/pages/signupPage.tsx
+++ client/src/pages/signupPage.tsx
@@ -8,3 +8,19 @@ const SignUpPage = () => {
     const [password, setPassword] = useState('');
     const [errorMessage, setErrorMessage] = useState('');
     const navigate = useNavigate();
+
+    const isEmailValid = (email: string) => {
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return emailRegex.test(email);
+      };
+    
+      const handleSubmit = async (e: React.FormEvent) => {
+        e.preventDefault();
+    
+        // Email validation
+        if (!isEmailValid(email)) {
+          setErrorMessage('Please enter a valid email address.');
+          return;
+        }
+
+        
