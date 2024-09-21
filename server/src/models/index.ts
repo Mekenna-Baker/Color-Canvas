9a4f992355b01c26d054b7e173de0192c9af42ef
mbaker <mekenna.drabner@gmail.com>
2024-09-20 21:54:13 -0600

    troubleshooting

--- server/src/models/index.ts
+++ server/src/models/index.ts
@@ -5,7 +5,7 @@ import { Sequelize } from 'sequelize';
 import { UserFactory } from './user';
 import { ImageFactory } from './image';
 
- 
+
 
 console.log("DB Name:", process.env.DB_NAME);
 console.log("DB User:", process.env.DB_USER);
@@ -13,23 +13,23 @@ console.log("DB User:", process.env.DB_USER);
 //initialize the Sequelize instance
 
 const sequelize = process.env.DB_URL
-  ? new Sequelize(process.env.DB_URL)
-  : new Sequelize(process.env.DB_NAME || '', process.env.DB_USER || '', process.env.DB_PASSWORD, {
-      host: 'localhost',
-      dialect: 'postgres',
-      dialectOptions: {
-        decimalNumbers: true,
-      },
+    ? new Sequelize(process.env.DB_URL)
+    : new Sequelize(process.env.DB_NAME || '', process.env.DB_USER || '', process.env.DB_PASSWORD, {
+        host: 'localhost',
+        dialect: 'postgres',
+        dialectOptions: {
+            decimalNumbers: true,
+        },
     });
 
 const User = UserFactory(sequelize);
 const Image = ImageFactory(sequelize);
 
-User.hasMany(Image, { foreignKey: 'userId', as: 'images',});
+User.hasMany(Image, { foreignKey: 'userId', as: 'images', });
 
 sequelize.sync({ alter: true }).then(() => {
     console.log('Database & tables created!');
-  });
+});
 
 
 export { sequelize, User, Image };
