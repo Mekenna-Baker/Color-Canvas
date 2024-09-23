d52b3022b7e2444eda866ccfedae9897d5193fc1
JoshuaPruitt <JoshuaPruitt6484@gmail.com>
2024-09-23 16:20:42 -0600

    added dev logout button to the error page

--- client/src/pages/errorPage.tsx
+++ client/src/pages/errorPage.tsx
@@ -1,12 +1,16 @@
-// import React from 'react';
-// import { Link } from 'react-router-dom';
-// import './errorPage.css';
+import React from 'react';
+import auth from '../utils/auth';
+import '../style/errorPage.css';
 
 const Error: React.FC = () => {
     return (
         <div>
             <h1>Error!</h1>
             <p>Im sorry, there was an error loading the page.</p>
+
+            <div>
+                <button onClick={auth.logout}>Logout</button>
+            </div>
         </div>
     )
 }
