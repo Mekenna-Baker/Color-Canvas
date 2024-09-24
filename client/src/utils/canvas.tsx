b6f96c30ad5dbef9e5e1b6f556ba8771fae21d3a
mbaker <mekenna.drabner@gmail.com>
2024-09-24 14:35:56 -0600

    edited canvas page to add labels to dimensions, and move buttons. Styled canvas page

--- client/src/utils/canvas.tsx
+++ client/src/utils/canvas.tsx
@@ -1,4 +1,4 @@
-import  React, { useRef, useState, useEffect, ChangeEvent} from "react"
+import React, { useRef, useState, useEffect, ChangeEvent } from "react"
 import colors2 from "../assets/colors";
 import { createImage } from "../api/imageAPI";
 import Auth from "./auth";
@@ -22,7 +22,7 @@ const CanvasComponent: React.FC = () => {
 
     //handles all selected colors on the page
     const [colorCode, setColorCode] = useState('#000000')
-    
+
 
     const changeColor = (color: string) => {
         setColorCode(color)
@@ -30,16 +30,16 @@ const CanvasComponent: React.FC = () => {
     }
 
     const changeColorCode = (e: ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
-        const {value} = e.target;
+        const { value } = e.target;
         setColorCode(value)
     }
 
 
     const handleWidthChange = (e: ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
-        const {name, value} = e.target;
+        const { name, value } = e.target;
         const valueInt = parseInt(value) * pixelSize
         canvasWidth = valueInt;
-        
+
         setDimensionData({
             ...dimensionData,
             [name]: value,
@@ -47,10 +47,10 @@ const CanvasComponent: React.FC = () => {
     };
 
     const handleHeightChange = (e: ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
-        const {name, value} = e.target;
+        const { name, value } = e.target;
         const valueInt = parseInt(value) * pixelSize
         canvasHeight = valueInt;
-        
+
         setDimensionData({
             ...dimensionData,
             [name]: value,
@@ -60,8 +60,8 @@ const CanvasComponent: React.FC = () => {
 
     //draws an initial grid onto the canvas.
     const drawGrid = (ctx: CanvasRenderingContext2D) => {
-        for (let x= 0; x< canvasWidth; x += pixelSize){
-            for(let y=0; y < canvasHeight; y += pixelSize){
+        for (let x = 0; x < canvasWidth; x += pixelSize) {
+            for (let y = 0; y < canvasHeight; y += pixelSize) {
                 ctx.fillStyle = '#ffffff';
                 ctx.fillRect(x, y, pixelSize, pixelSize);
 
@@ -73,10 +73,10 @@ const CanvasComponent: React.FC = () => {
 
     const drawPixel = (ctx: CanvasRenderingContext2D, x: number, y: number) => {
         //grabs the mouse click coordinates
-        const pixelX = Math.floor(x /pixelSize) * pixelSize
-        const pixelY = Math.floor(y /pixelSize) * pixelSize;
+        const pixelX = Math.floor(x / pixelSize) * pixelSize
+        const pixelY = Math.floor(y / pixelSize) * pixelSize;
 
-        
+
 
         ctx.fillStyle = colorCode;
         ctx.fillRect(pixelX, pixelY, pixelSize, pixelSize);
@@ -84,8 +84,8 @@ const CanvasComponent: React.FC = () => {
 
     const erasePixel = (ctx: CanvasRenderingContext2D, x: number, y: number) => {
         //grab the mouse canvas coordinates
-        const pixelX = Math.floor(x /pixelSize) * pixelSize;
-        const pixelY = Math.floor(y /pixelSize) * pixelSize;
+        const pixelX = Math.floor(x / pixelSize) * pixelSize;
+        const pixelY = Math.floor(y / pixelSize) * pixelSize;
 
         //clear the selected pixel
         ctx.clearRect(pixelX, pixelY, pixelSize, pixelSize)
@@ -100,10 +100,10 @@ const CanvasComponent: React.FC = () => {
 
     const handleMouseDown = (event: React.MouseEvent<HTMLCanvasElement>) => {
         const canvas = canvasRef.current;
-        if(!canvas) return;
+        if (!canvas) return;
 
         const ctx = canvas.getContext('2d');
-        if(!ctx) return;
+        if (!ctx) return;
 
         // Grabs the canvas's bounding retangle and the mouse click Coordinates
         const rect = canvas.getBoundingClientRect();
@@ -111,8 +111,8 @@ const CanvasComponent: React.FC = () => {
         const y = event.clientY - rect.top;
 
         //if the eraser is activated then set pixels to clear
-        if(isClear){
-            erasePixel(ctx, x ,y)
+        if (isClear) {
+            erasePixel(ctx, x, y)
             setPainting(true);
         } else {
             drawPixel(ctx, x, y);
@@ -121,20 +121,20 @@ const CanvasComponent: React.FC = () => {
     }
 
     const handleMouseMove = (event: React.MouseEvent<HTMLCanvasElement>) => {
-        if(!isPainting) return;
+        if (!isPainting) return;
 
         const canvas = canvasRef.current;
-        if(!canvas) return;
+        if (!canvas) return;
 
         const ctx = canvas.getContext('2d');
-        if(!ctx) return;
+        if (!ctx) return;
 
         const rect = canvas.getBoundingClientRect();
         const x = event.clientX - rect.left;
         const y = event.clientY - rect.top;
 
-        if(isClear){
-            erasePixel(ctx, x ,y)
+        if (isClear) {
+            erasePixel(ctx, x, y)
         } else {
             drawPixel(ctx, x, y)
         }
@@ -146,7 +146,7 @@ const CanvasComponent: React.FC = () => {
 
     const clearCanvas = () => {
         const ctx = canvasRef.current?.getContext('2d');
-        if(!ctx) return;
+        if (!ctx) return;
 
         //clear the grid completly
         ctx.clearRect(0, 0, canvasHeight, canvasWidth);
@@ -156,33 +156,33 @@ const CanvasComponent: React.FC = () => {
 
     const uploadCanvas = async () => {
         console.log('Uploading...')
-        
+
         try {
             //grab the users data by username
             const usernameData = Auth.getProfile()
-            const idData =  await retrieveUser(usernameData.username)
-            
+            const idData = await retrieveUser(usernameData.username)
+
 
             const canvas = canvasRef.current;
 
-            if(!canvas) return;
-            
+            if (!canvas) return;
+
             //convert image to a base64 String
             const dataUrl = canvas?.toDataURL('image/png');
-            
+
             const imageName = window.prompt('What do you wanna name your Project?') || 'title'
             //send data to be uploaded
             const imageObj: any = {
                 title: imageName,
                 width: canvasWidth,
-                height: canvasHeight, 
+                height: canvasHeight,
                 imageData: dataUrl,
                 userId: idData.id,
             }
 
             //ask user if they wish to save the image to their computer
             const saveImage = window.confirm('Would you Like to save the image?')
-            if(saveImage){
+            if (saveImage) {
                 const link = document.createElement('a');
                 link.href = dataUrl;
                 link.download = imageName + '.png';
@@ -196,17 +196,17 @@ const CanvasComponent: React.FC = () => {
 
             window.location.assign('/')
 
-        } catch(err: any){
+        } catch (err: any) {
             console.error('Error Uploading image Data:', err)
         }
     }
 
     useEffect(() => {
         const canvas = canvasRef.current;
-        if(!canvas) return;
+        if (!canvas) return;
 
         const ctx = canvas.getContext('2d');
-        if(!ctx) return;
+        if (!ctx) return;
 
         drawGrid(ctx);
     }, [dimensionData]);
@@ -214,46 +214,47 @@ const CanvasComponent: React.FC = () => {
     return (
         <div className="canvasComponentContainer">
             <div className="canvasHolder">
-                <canvas id='paintMain' ref={canvasRef} width={canvasWidth} height={canvasHeight} onMouseDown={handleMouseDown} onMouseMove={handleMouseMove} onMouseUp={handleMouseUpLeave} onMouseLeave={handleMouseUpLeave} style={{border: '1px solid red'}}></canvas>
-            
+                <canvas id='paintMain' ref={canvasRef} width={canvasWidth} height={canvasHeight} onMouseDown={handleMouseDown} onMouseMove={handleMouseMove} onMouseUp={handleMouseUpLeave} onMouseLeave={handleMouseUpLeave} style={{ border: '1px solid red' }}></canvas>
+
             </div>
             <div className="componentsContainer">
 
                 <div className="dimensionsContainer">
-                    <input type='text' name='width' value={dimensionData.width || ''} onChange={(e: any) => {handleWidthChange(e)}}></input>
-                    <input type='text' name='height' value={dimensionData.height || ''} onChange={(e: any) => {handleHeightChange(e)}}></input>
-                </div>
-
-                <div className="eraser"> 
-                    <button onClick={() => setClear(true)}>Eraser</button>
+                    <label className='width'>Width:</label>
+                    <input type='text' name='width' value={dimensionData.width || ''} onChange={(e: any) => { handleWidthChange(e) }}></input>
+                    <div className="dimensionsContainer">
+                        <label className='height'>Height:</label>
+                        <input type='text' name='height' value={dimensionData.height || ''} onChange={(e: any) => { handleHeightChange(e) }}></input>
+                    </div>
                 </div>
 
                 <div className="hexColorPicker">
                     {/* this is the hex color picker box */}
-                    <HexColorPicker color={colorCode} onChange={changeColor}/>
+                    <HexColorPicker color={colorCode} onChange={changeColor} />
                 </div>
 
                 <div className="colorCodeContainer">
-                    <label>Hex Code: <input type='text' name='colorCode' value={colorCode || ''} onChange={changeColorCode}></input></label>
+                    <label className='hex'>Hex Code: <input type='text' name='colorCode' value={colorCode || ''} onChange={changeColorCode}></input></label>
                 </div>
                 <div className="colorSelectors">
-                    {colors2.map((color: {index: number, color: string, colorName: string}) => (
-                        <button key={color.index} className='colors' style={{backgroundColor: color.color}} onClick={() => {changeColor(color.color); setClear(false)}}></button>
+                    {colors2.map((color: { index: number, color: string, colorName: string }) => (
+                        <button key={color.index} className='colors' style={{ backgroundColor: color.color }} onClick={() => { changeColor(color.color); setClear(false) }}></button>
                     ))}
                 </div>
 
-                <div className="clear">
-                    <button onClick={clearCanvas}>Clear Canvas</button>
+                <div className="clear-erase">
+                    <button className='clear'onClick={clearCanvas}>Clear</button>
+                    <button className='erase'onClick={() => setClear(true)}>Eraser</button>
                 </div>
 
-                <div className="uploadContainer">
-                    <button onClick={uploadCanvas}>Upload</button>
+                <div className="saveContainer">
+                    <button className='save'onClick={uploadCanvas}>Save</button>
                 </div>
             </div>
-            
+
 
         </div>
-        
+
     )
 }
 
